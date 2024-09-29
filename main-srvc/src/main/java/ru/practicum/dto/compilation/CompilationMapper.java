71d25928398ef28cc877995f0169ce697b08ea0e
Aleksey <aleksiei.ivin.97@yandex.ru>
2024-09-29 03:14:25 +0300

    refactor:поправить мапперы

--- main-srvc/src/main/java/ru/practicum/dto/compilation/CompilationMapper.java
+++ main-srvc/src/main/java/ru/practicum/dto/compilation/CompilationMapper.java
@@ -5,7 +5,7 @@ import ru.practicum.model.compilation.Compilation;
 public class CompilationMapper {
     public static CompilationOutDto toCompOutDto(Compilation compilation) {
         CompilationOutDto compilationOutDto = new CompilationOutDto();
-        compilationOutDto.setTitle(compilationOutDto.getTitle());
+        compilationOutDto.setTitle(compilation.getTitle());
         compilationOutDto.setPinned(compilation.getPinned());
         return compilationOutDto;
     }
